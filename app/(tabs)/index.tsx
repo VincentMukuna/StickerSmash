7ee33c29a0fc25f9910446f461d5eb0939a7bc7c
Vincent Mukuna <mukunavincent28@gmail.com>
2024-11-11 15:59:30 +0300

    handle platform differences

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,11 +5,12 @@ import EmojiPicker from "@/components/EmojiPicker";
 import EmojiSticker from "@/components/EmojiSticker";
 import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
+import domtoimage from "dom-to-image";
 import { ImageSource } from "expo-image";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import { useRef, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { captureRef } from "react-native-view-shot";
 
@@ -51,18 +52,35 @@ export default function Index() {
 	};
 
 	const onSaveImageAsync = async () => {
-		try {
-			const localUri = await captureRef(imageRef, {
-				height: 440,
-				quality: 1,
-			});
+		if (Platform.OS !== "web") {
+			try {
+				const localUri = await captureRef(imageRef, {
+					height: 440,
+					quality: 1,
+				});
 
-			await MediaLibrary.saveToLibraryAsync(localUri);
-			if (localUri) {
-				alert("Saved!");
+				await MediaLibrary.saveToLibraryAsync(localUri);
+				if (localUri) {
+					alert("Saved!");
+				}
+			} catch (e) {
+				console.log(e);
 			}
-		} catch (e) {
-			console.log(e);
+		} else {
+			try {
+				const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+					quality: 0.95,
+					width: 320,
+					height: 440,
+				});
+
+				let link = document.createElement("a");
+				link.download = "sticker-smash.jpeg";
+				link.href = dataUrl;
+				link.click();
+			} catch (error) {
+				console.log(error);
+			}
 		}
 	};
 
